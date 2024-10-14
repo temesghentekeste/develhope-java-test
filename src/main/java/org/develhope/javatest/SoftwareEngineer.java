134187f1045bca6ab2cc2572e97001641af9a923
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-14 17:26:52 +0300

    SoftwareEngineer added fields

--- src/main/java/org/develhope/javatest/SoftwareEngineer.java
+++ src/main/java/org/develhope/javatest/SoftwareEngineer.java
@@ -1,4 +1,28 @@
 package org.develhope.javatest;
 
-public class SoftwareEngineer {
+public class SoftwareEngineer extends OfficeJob{
+    private String company;
+    private int yearsOfExperience;
+
+    public SoftwareEngineer(String name, String jobTitle, String officeLocation, boolean remoteWorkOption, String company, int yearsOfExperience) {
+        super(name, jobTitle, officeLocation, remoteWorkOption);
+        this.company = company;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
