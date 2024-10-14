369b4791f37f8b9bffc7789c27c6faca61739c72
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-14 17:12:08 +0300

    Add job interface and OfficeJob class

---
+++ src/main/java/org/develhope/javatest/Job.java
@@ -0,0 +1,18 @@
+package org.develhope.javatest;
+
+public interface Job {
+    /*
+    Create Job Interface which have salary, startShift,
+endShift, printJobDescription, printWorkers, hirePerson,
+firePerson functions.
+     */
+    public double salary = 0;
+    void startShift();
+    void endShift();
+    void printJobDescription();
+    void printWorkers();
+    void hirePerson();
+    void firePerson();
+
+
+}
